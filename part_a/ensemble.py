e61d338da2985e9627aa39d100cd068e75b3c95b
John Song <johnsoong216@hotmail.com>
2020-12-09 17:17:55 -0500

    finish part A

--- part_a/ensemble.py
+++ part_a/ensemble.py
@@ -1,1 +1,170 @@
 # TODO: complete this file.
+import numpy as np
+from utils import *
+from part_a.knn import *
+from part_a.item_response import *
+from part_a.neural_network import *
+
+def bootstrap_idx(data, num_resamples):
+    return data[np.random.randint(0, len(data), (num_resamples, len(data)))]
+
+
+def knn(matrix, k):
+    nbrs = KNNImputer(n_neighbors=k)
+    # We use NaN-Euclidean distance measure.
+    mat = nbrs.fit_transform(matrix)
+    return mat
+
+def ensemble_predict(data, knn_matrix, theta, beta, nn_zero_matrix, nn_model, threshold=0.5):
+
+    knn_preds = knn_predict(data, knn_matrix)
+
+    irt_preds = irt_predict(data, theta, beta)
+
+    nn_preds = nn_predict(nn_zero_matrix, data, nn_model)
+
+    return ((nn_preds + irt_preds + knn_preds)/3) >= threshold
+
+
+def nn_predict(zero_data, valid_data, nn_model):
+    nn_model.eval()
+    nn_preds = []
+    for i, u in enumerate(valid_data["user_id"]):
+        inputs = Variable(zero_data[u]).unsqueeze(0)
+        output = nn_model(inputs)
+
+        guess = output[0][valid_data["question_id"][i]].item()
+        nn_preds.append(guess)
+    nn_preds = np.array(nn_preds)
+    return nn_preds
+
+
+def irt_predict(valid_data, theta, beta):
+    irt_preds = []
+    for i, q in enumerate(valid_data["question_id"]):
+        u = valid_data["user_id"][i]
+        x = (theta[u] - beta[q]).sum()
+        p_a = sigmoid(x)
+        irt_preds.append(p_a)
+    irt_preds = np.array(irt_preds)
+    return irt_preds
+
+
+def knn_predict(valid_data, knn_matrix):
+    knn_preds = []
+    for i in range(len(valid_data["user_id"])):
+        cur_user_id = valid_data["user_id"][i]
+        cur_question_id = valid_data["question_id"][i]
+        knn_preds.append(knn_matrix[cur_user_id, cur_question_id])
+    knn_preds = np.array(knn_preds)
+    return knn_preds
+
+
+def evaluate(data, pred):
+    return np.sum((data["is_correct"] == np.array(pred))) \
+    / len(data["is_correct"])
+
+
+
+
+
+
+def main():
+
+    # Load data
+    train_matrix = load_train_sparse("../data").toarray()
+    train_data = load_train_csv("../data")
+    val_data = load_valid_csv("../data")
+    test_data = load_public_test_csv("../data")
+    #
+    # Set up new datasets
+    num_resamples = 3
+    resampled_train_matrix = bootstrap_idx(train_matrix, num_resamples)
+
+    # resampled_train_matrix = np.concatenate([train_matrix, train_matrix, train_matrix]).reshape(3, len(train_matrix), -1)
+    # KNN
+    knn_matrix = knn(resampled_train_matrix[0], k=11)
+
+    # Item Response Theory
+    lr = 0.01
+    iterations = 20
+    theta, beta, val_log_likelihood, train_log_likelihood = \
+        irt(resampled_train_matrix[1], train_data, val_data, lr, iterations)
+
+    # Neural Network
+    k = 100
+    lr = 0.01
+    epoch = 40
+    lamb = 0.01
+
+    nn_zero_train_matrix = resampled_train_matrix[2].copy()
+    nn_zero_train_matrix[np.isnan(resampled_train_matrix[2])] = 0
+    nn_zero_train_matrix = torch.FloatTensor(nn_zero_train_matrix)
+    nn_train_matrix = torch.FloatTensor(resampled_train_matrix[2])
+
+    nn_model = AutoEncoder(resampled_train_matrix[2].shape[1], k)
+    epochs, train_losses, val_accs = train(nn_model, lr, nn_train_matrix, nn_zero_train_matrix,
+                                           val_data, epoch, lamb)
+
+    threshold = 0.5
+
+    ensemble_train_pred = ensemble_predict(train_data, knn_matrix, theta, beta, nn_zero_train_matrix, nn_model, threshold)
+    ensemble_train_acc = evaluate(train_data, ensemble_train_pred)
+
+    ensemble_val_pred = ensemble_predict(val_data, knn_matrix, theta, beta, nn_zero_train_matrix, nn_model, threshold)
+    ensemble_val_acc = evaluate(val_data, ensemble_val_pred)
+
+    ensemble_test_pred = ensemble_predict(test_data, knn_matrix, theta, beta, nn_zero_train_matrix, nn_model, threshold)
+    ensemble_test_acc = evaluate(test_data, ensemble_test_pred)
+
+    print("Ensemble Train accuracy: ", ensemble_train_acc)
+    print("Ensemble Validation accuracy: ", ensemble_val_acc)
+    print("Ensemble Test accuracy: ", ensemble_test_acc)
+
+    # Comparison with Individual Accuracy
+    train_knn_pred = knn_predict(train_data, knn_matrix) >= threshold
+    train_irt_pred = irt_predict(train_data, theta, beta) >= threshold
+    train_nn_pred = nn_predict(nn_zero_train_matrix, train_data, nn_model) >= threshold
+
+    val_knn_pred = knn_predict(val_data, knn_matrix) >= threshold
+    val_irt_pred = irt_predict(val_data, theta, beta) >= threshold
+    val_nn_pred = nn_predict(nn_zero_train_matrix, val_data, nn_model) >= threshold
+
+    test_knn_pred = knn_predict(test_data, knn_matrix) >= threshold
+    test_irt_pred = irt_predict(test_data, theta, beta) >= threshold
+    test_nn_pred = nn_predict(nn_zero_train_matrix, test_data, nn_model) >= threshold
+
+    knn_train_acc = evaluate(train_data, train_knn_pred)
+    knn_val_acc = evaluate(val_data, val_knn_pred)
+    knn_test_acc = evaluate(test_data, test_knn_pred)
+    print("KNN Train accuracy: ", knn_train_acc)
+    print("KNN Validation accuracy: ", knn_val_acc)
+    print("KNN Test accuracy: ", knn_test_acc)
+
+    irt_train_acc = evaluate(train_data, train_irt_pred)
+    irt_val_acc = evaluate(val_data, val_irt_pred)
+    irt_test_acc = evaluate(test_data, test_irt_pred)
+    print("IRT Train accuracy: ", irt_train_acc)
+    print("IRT Validation accuracy: ", irt_val_acc)
+    print("IRT Test accuracy: ", irt_test_acc)
+
+    nn_train_acc = evaluate(train_data, train_nn_pred)
+    nn_val_acc = evaluate(val_data, val_nn_pred)
+    nn_test_acc = evaluate(test_data, test_nn_pred)
+    print("NN Train accuracy: ", nn_train_acc)
+    print("NN Validation accuracy: ", nn_val_acc)
+    print("NN Test accuracy: ", nn_test_acc)
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
--- part_a/neural_network.py
+++ part_a/neural_network.py
@@ -8,6 +8,7 @@ import torch.utils.data
 
 import numpy as np
 import torch
+import matplotlib.pyplot as plt
 
 
 def load_data(base_path="../data"):
@@ -100,6 +101,11 @@ def train(model, lr, train_data, zero_train_data, valid_data, num_epoch, lamb=0)
     optimizer = optim.SGD(model.parameters(), lr=lr)
     num_student = train_data.shape[0]
 
+    epoch_list = []
+    train_loss_list = []
+    valid_acc_list = []
+
+
     for epoch in range(0, num_epoch):
         train_loss = 0.
 
@@ -123,6 +129,11 @@ def train(model, lr, train_data, zero_train_data, valid_data, num_epoch, lamb=0)
         valid_acc = evaluate(model, zero_train_data, valid_data)
         print("Epoch: {} \tTraining Cost: {:.6f}\t "
               "Valid Acc: {}".format(epoch, train_loss, valid_acc))
+        epoch_list.append(epoch)
+        train_loss_list.append(train_loss)
+        valid_acc_list.append(valid_acc)
+    return epoch_list, train_loss_list, valid_acc_list
+
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -167,18 +178,71 @@ def main():
     # Q2c
     num_question = train_matrix.shape[1]
     k_list = [10, 50, 100, 200, 500]
-    lr = 0.01
-    num_epoch = 10
+    lr_list = [0.001, 0.01, 0.1]
+    num_epoch_list = [5, 20, 40]
+    fig, ax = plt.subplots(nrows=len(num_epoch_list), ncols=len(lr_list), figsize=(18,12))
+    for i, num_epoch in enumerate(num_epoch_list):
+        for j, lr in enumerate(lr_list):
+            for k in k_list:
+                model = AutoEncoder(num_question, k)
+                # Set optimization hyperparameters.
+                epochs, train_losses, val_accs = train(model, lr, train_matrix, zero_train_matrix,
+                  valid_data, num_epoch)
 
-    for k in k_list:
-        model = AutoEncoder(num_question, k)
-        # Set optimization hyperparameters.
-        train(model, lr, train_matrix, zero_train_matrix,
-              valid_data, num_epoch)
+                ax[i][j].plot(epochs, val_accs, label=f"K = {k}");
+                ax[i][j].set_xticks(np.arange(0, num_epoch, num_epoch//5));
+                ax[i][j].set_ylabel("Validation Accuracy %")
+                ax[i][j].set_title(f"Epoch: {num_epoch}, LR: {lr}", fontsize=10);
+                fig.savefig("../figs/Q3c.png");
+
+            ax[i][j].legend(loc='upper left', prop={'size': 6});
+    fig.savefig("../figs/Q3c.png");
 
     # Q2d
+    k_star = 100
+    lr_star = 0.01
+    epoch_star = 50
+    model_star = AutoEncoder(num_question, k_star)
+    epochs, train_losses, val_accs = train(model_star, lr_star, train_matrix, zero_train_matrix,
+                  valid_data, epoch_star)
+
+    fig, ax = plt.subplots(ncols=2, figsize=(12, 28));
+    # Plot Train Loss
+    ax[0].plot(epochs, train_losses, label="train loss");
+    ax[0].set_title("Train Loss by Iteration");
+    ax[0].set_xlabel("Epoch");
+    ax[0].set_ylabel("Loss");
+
+    # Plot Validation Accuracy
+    ax[1].plot(epochs, val_accs, label="validation");
+    ax[1].set_title("Validation Accuracy by Iteration");
+    ax[1].set_xlabel("Epoch");
+    ax[1].set_ylabel("Accuracy");
+
+    fig.savefig("../figs/Q3d.png");
+
     # Q2e
     lamb_list = [0.001, 0.01, 0.1, 1]
+    fig, ax = plt.subplots(ncols=2, figsize=(12, 28));
+    for lamb in lamb_list:
+        model_star = AutoEncoder(num_question, k_star)
+        epochs, train_losses, val_accs = train(model_star, lr_star, train_matrix, zero_train_matrix,
+                                           valid_data, epoch_star, lamb)
+        ax[0].plot(epochs, train_losses, label=f"lamb={lamb}");
+        ax[1].plot(epochs, val_accs, label=f"lamb={lamb}");
+
+    ax[0].set_title("Train Loss by Iteration");
+    ax[0].set_xlabel("Epoch");
+    ax[0].set_ylabel("Loss");
+    ax[0].legend();
+
+    ax[1].set_title("Validation Accuracy by Iteration");
+    ax[1].set_xlabel("Epoch");
+    ax[1].set_ylabel("Accuracy");
+    ax[1].legend();
+    fig.savefig("../figs/Q3e.png");
+
+
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
